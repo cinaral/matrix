cb502153abbd77cb82fb1f6acfb4a10f70db768a
cinaral <alevent.cinar@gmail.com>
2022-09-06 02:10:32 -0500

    Add matrix.hpp

---
+++ README.md
@@ -0,0 +1,2 @@
+# ```matrix```: Basic matrix operations
+Place-holder matrix library for my other projects
---
+++ test/matlab/run_all_tests.m
@@ -0,0 +1,3 @@
+if ~exist('../../build/bin', 'dir')
+	error('Test binaries are missing. Use CMake to build the tests.')
+end
