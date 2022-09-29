e8dc93b5685deb1ed70dd35efa6168404aac73ba
cinaral <alevent.cinar@gmail.com>
2022-09-29 12:35:05 -0500

    Update .clang-format

--- include/matrix_op/row_operations.hpp
+++ include/matrix_op/row_operations.hpp
@@ -6,15 +6,18 @@
 namespace matrix_op
 {
 //* selects a row from an N_ROW by M_COL matrix
-//* returns a pointer to an array, (*)[M_COL]
+//* returns a pointer to array (*)[M_COL]
 //*
 //* inputs:
 //* 1. row_idx - row number
-//* 2. arr - N_ROW by M_COL matrix
+//* 2. mat - N_ROW by M_COL matrix
+//*
+//* usage:
+//* const real_t (&row) = *matrix_op::select_row<N_ROW, M_COL>(row_idx, mat);
 template <uint_t N_ROW, uint_t M_COL>
-static const real_t (*select_row(const uint_t row_idx, const real_t (&arr)[N_ROW * M_COL]))[M_COL]
+static const real_t (*select_row(const uint_t row_idx, const real_t (&mat)[N_ROW * M_COL]))[M_COL]
 {
-	const real_t(*row_ptr)[M_COL] = (real_t(*)[M_COL])(arr + row_idx * M_COL);
+	const real_t(*row_ptr)[M_COL] = (real_t(*)[M_COL])(mat + row_idx * M_COL);
 	return row_ptr;
 }
 
@@ -22,15 +25,17 @@ static const real_t (*select_row(const uint_t row_idx, const real_t (&arr)[N_ROW
 //*
 //* inputs:
 //* 1. row_idx - row number
-//* 2. sel - M_COL vector
+//* 2. row - M_COL vector
+//* usage:
+//* matrix_op::replace_row<N_ROW, [OPTIONAL] M_COL>(row_idx, row, mat);
 template <uint_t N_ROW, uint_t M_COL>
 static void
-replace_row(const uint_t row_idx, const real_t (&sel)[M_COL], real_t (&arr)[N_ROW * M_COL])
+replace_row(const uint_t row_idx, const real_t (&row)[M_COL], real_t (&mat)[N_ROW * M_COL])
 {
-	//* faster than std::memmove(arr + row_idx * M_COL, sel, M_COL * sizeof(real_t));
-	//* identical to: *arr + row_idx * M_COL + i = *sel + i
+	//* faster than std::memmove(mat + row_idx * M_COL, row, M_COL * sizeof(real_t));
+	//* identical to: *mat + row_idx * M_COL + i = *row + i
 	for (uint_t i = 0; i < M_COL; ++i) {
-		arr[row_idx * M_COL + i] = sel[i];
+		mat[row_idx * M_COL + i] = row[i];
 	}
 }
 } // namespace matrix_op
